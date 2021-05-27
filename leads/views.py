15886435104c6d4300a7714c1c53dacc4ade0092
glenchiridza <glenchiridz@gmail.com>
2021-05-27 22:49:24 +0200

    template root

--- leads/views.py
+++ leads/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("Hello hi glen")
+    # return HttpResponse("Hello hi glen")
+    # return render(request, 'leads/home_page.html')
+    context = {
+        "name": "glen",
+        "age": 23
+    }
+    return render(request, "second_page.html", context)
