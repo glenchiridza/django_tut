8b2d21cb9fad090c4ea63686826760051296231b
glenchiridza <glenchiridz@gmail.com>
2021-05-28 15:27:59 +0200

    static sfiles

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
+from django.conf.urls.static import static,settings
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/',include('leads.urls', namespace="leads"))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
