bc513bb9fff28ea6480ba3db4a1c90385e5f6d7f
glenchiridza <glenchiridz@gmail.com>
2021-05-28 13:30:48 +0200

    forms

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
 def lead_list(request):
@@ -10,5 +11,33 @@ def lead_list(request):
 
 
 def lead_detail(request, pk):
-    print(pk)
-    return HttpResponse("detail view now")
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, 'leads/lead_detail.html', context)
+
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print('form is valid')
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            print("lead created")
+            return redirect("/leads")
+    context = {
+        "form": form
+    }
+    return render(request, 'leads/lead_create.html', context)
