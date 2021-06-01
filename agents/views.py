bb865ea613a67f8c9ea2ec204ca4e17678c26c7f
glenchiridza <glenchiridz@gmail.com>
2021-06-01 20:42:19 +0200

    agents and leads

--- agents/views.py
+++ agents/views.py
@@ -10,7 +10,8 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = 'agents/agent_list.html'
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organization= self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
 
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
@@ -32,7 +33,8 @@ class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     context_object_name = 'agent'
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
 
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
@@ -43,12 +45,17 @@ class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
         return reverse('agents:agent-list')
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
 
 
 class AgentDeleteView(LoginRequiredMixin, generic.DetailView):
     template_name = 'agents/agent_delete.html'
     form_class = AgentModelForm
 
+    def get_success_url(self):
+        return reverse('agents:agent-list')
+
     def get_queryset(self):
-        return Agent.objects.all()
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
