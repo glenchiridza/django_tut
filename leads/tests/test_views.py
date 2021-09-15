db31fa72511c2365b6f2dcf10e6377b70865ae9a
glenchiridza <glenchiridz@gmail.com>
2021-09-15 20:14:35 +0200

    response

--- leads/tests/test_views.py
+++ leads/tests/test_views.py
@@ -5,7 +5,8 @@ from django.shortcuts import reverse
 class LandingPageTest(TestCase):
 
     def test_status_code(self):
-        request = self.client.get(reverse("landing page"))
+        response = self.client.get(reverse("landing page"))
+        print(response.content)
 
     def test_template_name(self):
         pass
