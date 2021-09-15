2d6e5ad834cfdf18d6fee5618cbd48b9addc6e50
glenchiridza <glenchiridz@gmail.com>
2021-09-15 20:19:10 +0200

    test in one

--- leads/tests/test_views.py
+++ leads/tests/test_views.py
@@ -4,13 +4,10 @@ from django.shortcuts import reverse
 
 class LandingPageTest(TestCase):
 
-    def test_status_code(self):
+    def test_get(self):
         response = self.client.get(reverse("landing-page"))
         print(response.content)
         print(response.status_code)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "landing_page.html")
 
-
-    def test_template_name(self):
-        pass
