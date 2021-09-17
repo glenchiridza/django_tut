f1ace27d7e0be87538208fa6c68c5903aa603388
glenchiridza <glenchiridz@gmail.com>
2021-09-17 20:53:15 +0200

    password reset complete

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
-    PasswordResetConfirmView
+    PasswordResetConfirmView, PasswordResetCompleteView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
@@ -17,6 +17,7 @@ urlpatterns = [
     path('logout/',LogoutView.as_view(),name="logout"),
     path('reset-password/',PasswordResetView.as_view(),name="reset-password"),
     path('password-reset-done/',PasswordResetDoneView.as_view(),name="password-reset-done"),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(),name="password-reset-complete"),
     path('password-reset-confirm/<uidb64>/<token>',PasswordResetConfirmView.as_view(),name="password-reset-confirm"),
 
 ]
