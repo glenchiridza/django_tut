57351e5eff0bb031d1939b041371d6014c01b749
glenchiridza <glenchiridz@gmail.com>
2021-09-17 20:34:51 +0200

    reset password link

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
@@ -14,6 +14,7 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
     path('login/',LoginView.as_view(),name="login"),
     path('logout/',LogoutView.as_view(),name="logout"),
+    path('reset-password/',PasswordResetView.as_view(),name="reset-password"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
