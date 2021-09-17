570a871e5cb880b9d45002b2efd20f391db4c16a
glenchiridza <glenchiridz@gmail.com>
2021-09-17 20:40:04 +0200

    email password reset done

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
@@ -15,6 +15,7 @@ urlpatterns = [
     path('login/',LoginView.as_view(),name="login"),
     path('logout/',LogoutView.as_view(),name="logout"),
     path('reset-password/',PasswordResetView.as_view(),name="reset-password"),
+    path('password-reset-done/',PasswordResetDoneView.as_view(),name="password-reset-done"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
